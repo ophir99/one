8dac6887e03c3599b8c48fc09c29978d56424ddc
Prasad Kadiyala <external.pvkadiyala@atheerair.com>
2019-02-23 10:06:34 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,23 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
+  let myObj;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ReactiveFormsModule
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    myObj = fixture.debugElement.componentInstance;
   }));
 
   it('should create the app', () => {
@@ -32,4 +38,27 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app-one!');
   });
+
+
+  it("title should be changed", ()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.debugElement.componentInstance;
+    app.changeName();
+    expect(app.title).toBe('React');
+  })
+
+  it("Form should be crated", ()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.debugElement.componentInstance;
+    app.createForm();
+    expect(app.myF).toBeTruthy();
+  })
+  it("Name => formcontrol should be invalid if it has more than 5 letters", ()=>{
+    console.log(myObj);
+    myObj.createForm();
+    myObj.myF.get('name').setValue('An');
+    expect(myObj.myF.get('name').valid).toBeTruthy();
+  })
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-one';
+  myF: FormGroup
+  constructor(
+    protected fB: FormBuilder
+  ){
+
+  }
+
+  changeName(){
+    this.title = "React";
+  }
+
+  createForm(){
+    this.myF = this.fB.group({
+      name: ['', [Validators.maxLength(5)]],
+      email: []
+    })
+  }
 }
